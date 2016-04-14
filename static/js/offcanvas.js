ee9b4ecf1c15afb407dfb6818dea6f30ef0e20f2
gaguilar <gavarela90@gmail.com>
2016-04-13 21:36:14 -0600

    uncomment scripts

--- static/js/offcanvas.js
+++ static/js/offcanvas.js
@@ -3,22 +3,22 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 
-$( "#api-accordion" ).click(function() {
-  $.ajax({
-  dataType: "json",
-  url: document.URL,
-  success: function( data ) {
+  $("#api-accordion").click(function () {
+    $.ajax({
+      dataType: "json",
+      url: document.URL,
+      success: function (data) {
         $('#api').html(JSON.stringify(data, undefined, 2));
-        $('pre code').each(function(i, e) {
-            console.log(e);
-            hljs.highlightBlock(e)
+        $('pre code').each(function (i, e) {
+          console.log(e);
+          hljs.highlightBlock(e)
         });
-    }
-});
-});
-$('.datepicker').datepicker();
-
-$(".chosen-select").chosen()
+      }
+    });
+  });
+  // $('.datepicker').datepicker();
+  //
+  // $(".chosen-select").chosen()
 
 
 });
