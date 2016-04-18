773d81740726c9a138f5865f1a406de6e13db565
gaguilar <gavarela90@gmail.com>
2016-04-17 20:44:34 -0600

    separate tree logic

--- static/src/js/location_list/index.jsx
+++ static/src/js/location_list/index.jsx
@@ -3,8 +3,8 @@ import './main.css';
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
-import * as actions from './actions/index';
+import configureStore from '../utils/treeAjax/store/configureStore';
+import * as actions from '../utils/treeAjax/actions/index';
 import App from './components/App';
 
 let data = window.__data;
